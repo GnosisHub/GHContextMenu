d11db379bf74e83be9963a96f1778991cdeccb86
tapasya <tapasyakhs@gmail.com>
2014-01-27 11:25:16 +0530

    Initial Commit

---
+++ GHContextMenu/GHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GHAppDelegate.h
+//  GHContextMenu
+//
+//  Created by Tapasya on 27/01/14.
+//  Copyright (c) 2014 Tapasya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GHContextMenu/GHViewController.h
@@ -0,0 +1,13 @@
+//
+//  GHViewController.h
+//  GHContextMenu
+//
+//  Created by Tapasya on 27/01/14.
+//  Copyright (c) 2014 Tapasya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GHViewController : UIViewController
+
+@end
